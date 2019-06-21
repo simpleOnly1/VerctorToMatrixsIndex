9b7e59750df873962f7973eca2e2471db9c01453
Zhou_Guang_Bin <35085763+simpleOnly1@users.noreply.github.com>
2019-06-21 22:08:57 +0800

    Update VerctorToMatrixsIndex.m

--- VerctorToMatrixsIndex.m
+++ VerctorToMatrixsIndex.m
@@ -1,6 +1,5 @@
-function c = VerctorToMatrixsIndex(s,a)
-% �����������һά����s�����ԭ���󷵻����Ӧ��ά����λ��(x,y)
-% a = [1 2 3;4 5 6;7 8 9;10 11 12];
+function c = VerctorToMatrixsIndex(a)
+% 将向量化后的一维坐标t，结合原矩阵返回其对应二维坐标位置(x,y)
 t = a(:);
 b = [0 0];
 [m n] = size(a);
@@ -14,6 +13,6 @@ for i = 1 : m*n
     end
 end
 b = [b(2:m*n+1,1) b(2:m*n+1,2)];
-% ��¼��ǰԪ�ض�Ӧ�Ķ�ά����λ�ò�����
+% 记录当前元素对应的二维坐标位置并返回
 c = [b(s,1) b(s,2)];
 end
